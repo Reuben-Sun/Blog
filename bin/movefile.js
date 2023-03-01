08ede4dd9c6be72d5913910bd1b6e26b862a6143
Reuben <2824016820@qq.com>
2023-03-02 01:39:04 +0800

    update movefile and delete theme

--- bin/movefile.js
+++ bin/movefile.js
@@ -1,14 +1,14 @@
 const fs = require("fs")
 const path = require("path")
 
+const srcArticles = path.resolve(__dirname, '..', 'blog-note', 'articles');
+const destArticles = path.resolve(__dirname, '..', 'source', '_posts');
 
-fs.renameSync(
-    path.resolve(__dirname, '..', 'blog-note', 'articles'),
-    path.resolve(__dirname, '..', 'source', '_posts')
-);
+fs.rmSync(destArticles, { recursive: true, force: true });
+fs.renameSync(srcArticles, destArticles);
 
+const srcImages = path.resolve(__dirname, '..', 'blog-note', 'images');
+const destImages = path.resolve(__dirname, '..', 'source', 'images');
 
-fs.renameSync(
-    path.resolve(__dirname, '..', 'blog-note', 'images'),
-    path.resolve(__dirname, '..', 'source', 'images')
-);
+fs.rmSync(destImages, { recursive: true, force: true });
+fs.renameSync(srcImages, destImages);
